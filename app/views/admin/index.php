e3783988d2945a5880e486c6f85b6b402d7fc097
Philipp Schüttlöffel <schuettloeffel@zqs.uni-hannover.de>
2020-12-22 15:55:05 +0100

    initial production commit

---
+++ app/views/admin/index.php
@@ -0,0 +1,12 @@
+<div class="container" id="app">
+    <h1 class="display-1 text-center">Starte Anwendung&hellip;</h1>
+</div>
+
+<script type="text/javascript">
+    let API_URL  = '<?= PluginEngine::getURL('meetingplugin', [], 'api') ?>';
+    let ICON_URL = '<?= Assets::url('images/icons/') ?>';
+</script>
+
+
+<? PageLayout::addScript($this->plugin->getPluginUrl() . '/static/main.9681e65b8a75d1433ed6.js'); ?>
+
---
+++ app/views/index/index.php
@@ -0,0 +1,14 @@
+<div class="container" id="app">
+    <h1 class="display-1 text-center">Starte Anwendung&hellip;</h1>
+</div>
+
+<script type="text/javascript">
+    let API_URL  = '<?= PluginEngine::getURL('meetingplugin', [], 'api', true) ?>';
+    let CID      = '<?= $cid ?>';
+    let ICON_URL = '<?= Assets::url('images/icons/') ?>';
+    let PLUGIN_ASSET_URL =  '<?= $plugin->getAssetsUrl() ?>';
+</script>
+
+
+<? PageLayout::addScript($this->plugin->getPluginUrl() . '/static/main.9681e65b8a75d1433ed6.js'); ?>
+
